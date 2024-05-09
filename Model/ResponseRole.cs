26d07c50a3265643484f1685d91ac31368adcc34
Mauricio Farfan <maufar402@gmail.com>
2024-05-09 11:55:52 -0600

    Logic API ResponseRole

--- Model/ResponseRole.cs
+++ Model/ResponseRole.cs
@@ -5,17 +5,21 @@ public class ResponseRole
 {
 	public string PersonA { get; set; }
 	public string PersonB { get; set; }
+	public string Date { get; set; }
 
 	public ResponseRole()
 	{
 		PersonA = "";
 		PersonB = "";
-	}
+        Date = "";
 
-    public ResponseRole(string personA, string personB)
+    }
+
+    public ResponseRole(string personA, string personB, string date)
     {
         PersonA = personA;
         PersonB = personB;
+        Date = date;
     }
 }
 
--- Repository/RepositoryRoleSoundConsole.cs
+++ Repository/RepositoryRoleSoundConsole.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using APIAutomation.Interfaces;
 using APIAutomation.Model;
+using Newtonsoft.Json;
 
 namespace APIAutomation.Repository;
 
@@ -10,7 +11,49 @@ public class RepositoryRoleSoundConsole : IRoleSoundConsole
 
     public ResponseRole GetTodayRole()
     {
-        return new ResponseRole("Abi", "Abihud");
+        try
+        {
+            string fileName = "Source/RoleSound.json";
+            string json = File.ReadAllText(fileName);
+
+            ResponseRole[]? roles = JsonConvert.DeserializeObject<ResponseRole[]>(json);
+
+            DateTime dateTimeUTC = DateTime.UtcNow;
+            TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+            DateTime dateTimeWithTimeZone = TimeZoneInfo.ConvertTimeFromUtc(dateTimeUTC, timeZone);
+
+            DayOfWeek dayOfWeek = dateTimeWithTimeZone.DayOfWeek;
+
+            if (dayOfWeek == DayOfWeek.Thursday)
+            {
+                string actualDate = dateTimeWithTimeZone.ToString("dd/MM/yyyy");
+                return ObtainRolePerDay(roles, actualDate);
+            }
+            else if (dayOfWeek == DayOfWeek.Saturday)
+            {
+                string tomorrowDate = dateTimeWithTimeZone.AddDays(1).ToString("dd/MM/yyyy");
+                return ObtainRolePerDay(roles, tomorrowDate);
+            }
+            else
+                return new ResponseRole();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading file. {ex.Message}");
+            return new ResponseRole();
+        }
+    }
+
+    private static ResponseRole ObtainRolePerDay(ResponseRole[] roles, string date)
+    {
+        foreach (ResponseRole role in roles)
+        {
+            if (role.Date.Equals(date))
+            {
+                return new ResponseRole(role.PersonA, role.PersonB, role.Date);
+            }
+        }
+        return new ResponseRole();
     }
 }
 
